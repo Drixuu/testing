7b5ca30d996166ca54ee095527b053f2cefe63eb
Rafal Adamiec <rafal.adamiec1@gmail.com>
2021-05-29 08:27:18 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("\n" + "Test - pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(21, 346);
+        int substractResult = calculator.subtract(345, 3);
+
+        if (addResult != 0) {
+            System.out.println("Result is: " + addResult);
+        } else {
+            System.out.println("Add result is 0");
+        }
+
+        if (substractResult != 0) {
+            System.out.println("Result is: " + substractResult);
+        } else {
+            System.out.println("Substraction result is 0");
+        }
     }
 }
